8d34c7fb04027feb1001f00a4595875c8ec24466
jv88899 <jv88899@gmail.com>
2019-03-10 18:41:04 -0500

    created PlayerCard component

--- att-five-app/src/components/App.js
+++ att-five-app/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PlayerCard from './playerCard/PlayerCard';
 
 class App extends Component {
     state = {
@@ -7,61 +8,71 @@ class App extends Component {
                 firstName: 'LeBron',
                 lastName: 'James',
                 fullName: 'LeBron James',
+                primaryPosition: 'SF',
                 currentTeamFullName: 'Los Angeles Lakers',
                 currentTeamNickname: 'Lakers',
                 pointsPerGame: 32.0,
                 reboundsPerGame: 11.0,
                 assistsPerGame: 12.0,
                 stealsPerGame: 2.5,
-                blocksPerGame: 2.7
+                blocksPerGame: 2.7,
+                currentImage: 'https://placehold.it/100x100'
             },
             {
                 firstName: 'Stephen',
                 lastName: 'Curry',
                 fullName: 'Stephen Curry',
+                primaryPosition: 'PG',
                 currentTeamFullName: 'Golden State Warriors',
                 currentTeamNickname: 'Warriors',
                 pointsPerGame: 31.0,
                 reboundsPerGame: 4.0,
                 assistsPerGame: 14.0,
                 stealsPerGame: 3.5,
-                blocksPerGame: 0.7
+                blocksPerGame: 0.7,
+                currentImage: 'https://placehold.it/100x100'
             },
             {
                 firstName: 'Kevin',
                 lastName: 'Durant',
                 fullName: 'Kevin Durant',
+                primaryPosition: 'PF',
                 currentTeamFullName: 'Golden State Warriors',
                 currentTeamNickname: 'Warriors',
                 pointsPerGame: 27.0,
                 reboundsPerGame: 13.0,
                 assistsPerGame: 8.0,
                 stealsPerGame: 2.1,
-                blocksPerGame: 2.6
+                blocksPerGame: 2.6,
+                currentImage: 'https://placehold.it/100x100'
             },
             {
                 firstName: 'Anthony',
                 lastName: 'Davis',
                 fullName: 'Anthony Davis',
+                primaryPosition: 'C',
                 currentTeamFullName: 'New Orleans Pelicans',
                 currentTeamNickname: 'Pelicans',
                 pointsPerGame: 22.0,
                 reboundsPerGame: 19.0,
                 assistsPerGame: 7.0,
                 stealsPerGame: 0.5,
-                blocksPerGame: 4.7
+                blocksPerGame: 4.7,
+                currentImage: 'https://placehold.it/100x100'
             },
             {
                 firstName: 'Joel',
                 lastName: 'Embid',
                 fullName: 'Joel Embid',
+                primaryPosition: 'C',
                 currentTeamFullName: 'Philadelphia 76ers',
                 currentTeamNickname: '76ers',
                 pointsPerGame: 24.0,
                 reboundsPerGame: 16.0,
-                assistsPerGame: 5..6,
+                assistsPerGame: 5.6,
                 stealsPerGame: 1.8,
-                blocksPerGame: 3.8
+                blocksPerGame: 3.8,
+                currentImage: 'https://placehold.it/100x100'
             },
         ]
     }
@@ -69,10 +80,23 @@ class App extends Component {
     render() {
         return(
             <div className="app">
-                This is from my App component
-                {
-                    console.log(this.state)
-                }
+                <div className="player__list">
+                    {
+                        this.state.database.map( player => (
+                            <PlayerCard
+                                currentImage={player.currentImage}
+                                fullName={player.fullName}
+                                primaryPosition={player.primaryPosition}
+                                currentTeamName={player.currentTeamFullName}
+                                pointsPerGame={player.pointsPerGame}
+                                reboundsPerGame={player.reboundsPerGame}
+                                assistsPerGame={player.assistsPerGame}
+                                blocksPerGame={player.blocksPerGame}
+                                stealsPerGame={player.stealsPerGame}
+                            />
+                        ))
+                    }
+                </div>
             </div>
         )
     }
---
+++ att-five-app/src/components/playerCard/PlayerCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './player-card.css';
+
+const PlayerCard = props => (
+    <div className="player__card">
+        <img
+            className="player__img img-responsive"
+            src={props.currentImage}
+            alt={'img of ' + props.currentImage}
+        />
+        <h3 className="player__card__name">{props.fullName}</h3>
+        <div className="player__card__info">
+            <p className="player__card__info--position">{props.primaryPosition}</p>
+            <p className="player__card__info--team">{props.currentTeamName}</p>
+        </div>
+        <div className="player__card__stats">
+            <div className="player__card__stats__points">
+                <p>Points:</p>
+                <p>{props.pointsPerGame}</p>
+            </div>
+            <div className="player__card__stats__rebounds">
+                <p>Rebounds:</p>
+                <p>{props.reboundsPerGame}</p>
+            </div>
+            <div className="player__card__stats__assists">
+                <p>Assists:</p>
+                <p>{props.assistsPerGame}</p>
+            </div>
+            <div className="player__card__stats__blocks">
+                <p>Blocks:</p>
+                <p>{props.blocksPerGame}</p>
+            </div>
+            <div className="player__card__stats__steals">
+                <p>Steals:</p>
+                <p>{props.stealsPerGame}</p>
+            </div>
+        </div>
+    </div>
+)
+
+export default PlayerCard;
